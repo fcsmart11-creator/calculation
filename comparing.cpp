d080d8deb67ca0d3727a9b8c3a6c60fa995a96a9
fcsmart11-creator <fcsmart11@gmail.com>
2026-06-18 07:08:06 +0000

    add comparing.cpp
    computing for 3 valus

---
+++ comparing.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 7;
+    int b = 6;
+    int c = 3;
+
+    if (a == b && b == c)
+        cout << "Equal";
+    else
+        cout << "Not equal";
+
+    return 0;
+}
