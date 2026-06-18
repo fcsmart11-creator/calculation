346d0e9d23236dc8ec1e17366912e35c0353dd8f
fcsmart11-creator <fcsmart11@gmail.com>
2026-06-18 00:05:27 +0000

    Add arithmetic code
    performing arithmetic operation

---
+++ arithmetic.cpp
@@ -0,0 +1,17 @@
+/*performing arithmetic operations*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 8;
+    int b = 4;
+
+    cout << "sum = " << (a + b) << endl;
+    cout << "difference = " << (a - b) << endl;
+    cout << "product = " << (a * b) << endl;
+    cout << "quotient = " << (a / b) << endl;
+    
+    return 0;
+}
