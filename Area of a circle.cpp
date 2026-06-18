f8f35924253545715e55ced597140a416db874a0
fcsmart11-creator <fcsmart11@gmail.com>
2026-06-18 00:27:20 +0000

    add area of a circle
    calculate the area of circle

---
+++ Area of a circle.cpp
@@ -0,0 +1,17 @@
+/*calculeting area of a circle*/
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+
+    double radius = 7;
+    const double pi = 3.14159;
+
+    cout << "radius = " << radius << endl;
+    cout << "pi = " << pi << endl;
+    cout << "area = " << pi * radius * radius << endl;
+
+    return 0;
+}
